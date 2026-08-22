24eef97753a55f4ac22d53bf74945c988d0cc686
Taylor Bryant <taylor@bryant.io>
2026-08-22 12:53:07 -0500

    Simplify canvas loading skeleton layout

--- app/(app)/w/[workspaceId]/c/[canvasId]/loading.tsx
+++ app/(app)/w/[workspaceId]/c/[canvasId]/loading.tsx
@@ -2,11 +2,8 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export default function CanvasLoading() {
 	return (
-		<div className="flex h-[calc(100svh-3rem)] min-h-96 flex-col">
-			<div className="flex h-12 shrink-0 items-center px-4">
-				<Skeleton className="h-6 w-48" />
-			</div>
-			<Skeleton className="min-h-0 flex-1 rounded-none" />
+		<div className="h-[calc(100svh-3rem)] min-h-96 border-border border-t">
+			<Skeleton className="h-full rounded-none" />
 		</div>
 	);
 }
